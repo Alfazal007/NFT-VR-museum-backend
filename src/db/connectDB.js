59445e834c8dcc52640a7d38c207e540a3185370
Alfazal007 <alfazal7777@gmail.com>
2023-12-24 18:21:57 +0530

    Update MongoDB connection string

--- src/db/connectDB.js
+++ src/db/connectDB.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-        const connectionInstance = await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`);
+        const connectionInstance = await mongoose.connect(`${process.env.MONGO_CONNECTION_STRING}/${DB_NAME}`);
         console.log(`\n MongoDB connected !! DB Host : ${connectionInstance.connection.host}`);
     } catch (err) {
         console.log("MongoDB connection error ", err);
