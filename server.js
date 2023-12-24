d005eeaae9c2bb5f08b37b289d3146fedb91d384
Alfazal007 <alfazal7777@gmail.com>
2023-12-24 16:11:39 +0530

    folder setup completed

--- server.js
+++ server.js
@@ -1,11 +1,3 @@
-const express = require('express');
-const app = express();
-const dotenv = require('dotenv').config();
+import { startServer } from "./src/index.js";
 
-app.get('/nft-vr/service/', (req, res) => {
-  return res.status(200).json({ message: 'Server is running' });
-});
-
-app.listen(process.env.PORT, () => {
-  console.log('Server is up and running');
-});
+startServer();
