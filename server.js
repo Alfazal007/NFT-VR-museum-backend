64d8f92f07944e34f7aa307fd752845e8dd9902c
rohith02b <rohith20b@gmail.com>
2023-12-26 23:40:10 +0530

    feat : mongo connection

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const app = express();
 const dotenv = require('dotenv').config();
+const mongoose = require('mongoose');
 
 app.get('/nft-vr/service/', (req, res) => {
   return res.status(200).json({ message: 'Server is running' });
 });
 
-app.listen(process.env.PORT, () => {
-  console.log('Server is up and running');
+app.listen(process.env.PORT, async () => {
+  try {
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGO_CONNECTION_STRING}`
+    );
+    console.log(
+      `MongoDB connected !! DB Host : ${connectionInstance.connection.host}`
+    );
+    console.log('Working');
+  } catch (err) {
+    console.log('MongoDB connection error ', err);
+  }
 });
