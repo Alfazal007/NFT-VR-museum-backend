f25e6d705634be575c60836a26cc1a99fd37fa14
rohith02b <rohith20b@gmail.com>
2023-12-28 19:52:41 +0530

    fix : dbConnect and base route

--- src/db/connectDB.js
+++ src/db/connectDB.js
@@ -3,11 +3,10 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-        const connectionInstance = await mongoose.connect(`${process.env.MONGO_CONNECTION_STRING}/${DB_NAME}`);
+        const connectionInstance = await mongoose.connect(`${process.env.MONGO_CONNECTION_STRING}`);
         console.log(`\n MongoDB connected !! DB Host : ${connectionInstance.connection.host}`);
     } catch (err) {
         console.log("MongoDB connection error ", err);
-        process.exit(1);
     }
 };
 
--- src/index.js
+++ src/index.js
@@ -5,16 +5,24 @@ dotenv.config({
     path: ".env",
 });
 
+app.get("/nft-vr/service/", (req, res) => {
+    return res.status(200).json({ message: "Working" });
+});
 
 const startServer = () => {
     connectDB()
         .then(() => {
             app.listen(process.env.PORT || 8000, () => {
-                console.log(`Server is running on port: ${process.env.PORT || 8000}`);
+                console.log(
+                    `Server is running on port: ${process.env.PORT || 8000}`
+                );
             });
         })
         .catch((err) => {
-            console.log("MongoDB connection error. Please make sure MongoDB is up and running. " + err);
+            console.log(
+                "MongoDB connection error. Please make sure MongoDB is up and running. " +
+                    err
+            );
         });
 };
 
