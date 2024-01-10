768ee657a1dae41db6f99c50970198cadf77a72a
Alfazal007 <alfazal7777@gmail.com>
2024-01-10 22:44:27 +0530

    fix: improved error return on update

--- src/controllers/usercontroller/updateUser.controller.js
+++ src/controllers/usercontroller/updateUser.controller.js
@@ -7,18 +7,22 @@ import { isLoggedIn } from "../../middlewares/auth.middleware.js";
 const updateUser = asyncHandler(
     async (req, res) => {
         const { name = req.user.name, email = req.user.email, role = req.user.role, phone = req.user.phone } = req.body;
-        const updatedUser = await User.findByIdAndUpdate(req.user?._id,
-            {
-                $set: {
-                    name: name,
-                    email: email,
-                    role: role,
-                    phone: phone
-                }
-            },
-            { new: true, runValidators: true },
-        ).select("-password -refreshToken");
-        return res.status(200).json(new ApiResponse(200, "Update successful", updatedUser));
+        try {
+            const updatedUser = await User.findByIdAndUpdate(req.user?._id,
+                {
+                    $set: {
+                        name: name,
+                        email: email,
+                        role: role,
+                        phone: phone
+                    }
+                },
+                { new: true, runValidators: true },
+            ).select("-password -refreshToken");
+            return res.status(200).json(new ApiResponse(200, "Update successful", updatedUser));
+        } catch (err) {
+            throw new ApiError(400, "Update failed try different credentials");
+        }
     }
 );
 
